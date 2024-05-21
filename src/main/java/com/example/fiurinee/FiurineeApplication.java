127ef3f05f1c87d938bb8f68221ca3ad16b2fe51
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-05-21 21:05:36 +0900

    Add files via upload

---
+++ src/main/java/com/example/fiurinee/FiurineeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiurinee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiurineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiurineeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fiurinee/FiurineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiurinee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiurineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
