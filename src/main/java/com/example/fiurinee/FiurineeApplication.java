7aa0667935ffb917a565f0749b9b0fe34c27cff9
jihyeon <qormoon@sookmyung.ac.kr>
2024-06-16 20:09:40 +0900

    feat: 실행시 csv파일 파싱 후 데이터베이스 저장

--- src/main/java/com/example/fiurinee/FiurineeApplication.java
+++ src/main/java/com/example/fiurinee/FiurineeApplication.java
@@ -1,15 +1,28 @@
 package com.example.fiurinee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.fiurinee.domain.flower.service.FlowerService;
 
 @SpringBootApplication
-public class FiurineeApplication {
+public class FiurineeApplication implements CommandLineRunner {
+
+	@Autowired
+	private FlowerService flowerService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FiurineeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// CSV 파일 경로
+		String filePath = "src/main/resources/flowers.csv";
+		// CSV 파일을 파싱하고 데이터베이스에 저장합니다.
+		flowerService.parseAndSaveCsv(filePath);
+	}
 }
 
 //http://localhost:8080/oauth2/authorization/kakao
