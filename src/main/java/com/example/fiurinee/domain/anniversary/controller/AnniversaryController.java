e2fec36bf6aec7d5ea4c9b52b73d4b6d2bfbc508
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-06-18 12:48:07 +0900

    feat: 기념일 Api

--- src/main/java/com/example/fiurinee/domain/anniversary/controller/AnniversaryController.java
+++ src/main/java/com/example/fiurinee/domain/anniversary/controller/AnniversaryController.java
@@ -1,22 +1,25 @@
 package com.example.fiurinee.domain.anniversary.controller;
 
+import com.example.fiurinee.domain.anniversary.controller.api.AnniversaryApi;
 import com.example.fiurinee.domain.anniversary.entity.Anniversary;
 import com.example.fiurinee.domain.anniversary.service.AnniversaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 @RestController
-public class AnniversaryController {
+public class AnniversaryController implements AnniversaryApi {
     @Autowired
     private AnniversaryService anniversaryService;
 
-    @PostMapping("/member/{memberId}/anniversary")
-    public Anniversary addAnniversary(@PathVariable Long memberId, @RequestBody Map<String, String> request) {
+    @Override
+    public ResponseEntity<Anniversary> addAnniversary(@PathVariable Long id, @RequestBody Map<String, String> request) {
         LocalDate date = LocalDate.parse(request.get("date"));
         String type = request.get("type");
-        return anniversaryService.addAnniversary(memberId, date, type);
+        Anniversary anniversary = anniversaryService.addAnniversary(id, date, type);
+        return ResponseEntity.ok(anniversary);
     }
 }
