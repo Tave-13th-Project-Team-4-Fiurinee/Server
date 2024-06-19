9a2111b2177e2191f8c8edac2abb7273bdab3dd7
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-06-19 16:14:47 +0900

    Update JwtVerifyFilter.java

--- src/main/java/com/example/fiurinee/global/security/filter/JwtVerifyFilter.java
+++ src/main/java/com/example/fiurinee/global/security/filter/JwtVerifyFilter.java
@@ -25,7 +25,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class JwtVerifyFilter extends OncePerRequestFilter {
     // 소셜 로그인 관련 URI 추가
-    private static final String[] whitelist = {"/signUp", "/login", "/refresh", "/", "/index.html", "/oauth2/login", "/login/oauth2/code/*", "/oauth2/authorization/kakao","/member/*/refresh"};
+    private static final String[] whitelist = {"/signUp", "/login", "/refresh", "/", "/index.html", "/oauth2/login", "/login/oauth2/code/*", "/oauth2/authorization/kakao","/member/*/refresh","/swagger-ui/index.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/v3/api-docs"};
     private final RedisUtil redisUtil;
 
     private static void checkAuthorizationHeader(String header) {
