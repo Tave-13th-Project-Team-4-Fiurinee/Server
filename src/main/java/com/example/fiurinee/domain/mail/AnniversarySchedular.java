f1e1c505c9f35b993724bcbb2c665183f0bdd7e0
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-06-27 23:24:22 +0900

    feat: 스케쥴러 추가

---
+++ src/main/java/com/example/fiurinee/domain/mail/AnniversarySchedular.java
@@ -0,0 +1,47 @@
+package com.example.fiurinee.domain.mail;
+
+import com.example.fiurinee.domain.anniversary.entity.Anniversary;
+import com.example.fiurinee.domain.anniversary.service.AnniversaryService;
+import com.example.fiurinee.domain.member.entity.Member;
+import com.example.fiurinee.domain.member.service.MemberService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AnniversarySchedular {
+    @Autowired
+    private AnniversaryService anniversaryService;
+
+    @Autowired
+    private MailService mailService;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Scheduled(cron = "0 27 23 * * *")
+    public void sendDDayZeroAnniversaryEmails() {
+        List<Member> members = memberService.findAll();
+        for (Member member : members) {
+            List<Anniversary> anniversaries = member.getAnniversaries();
+            for (Anniversary anniversary : anniversaries) {
+                List<Map<String, Integer>> allDDays = anniversaryService.calculateDDay(anniversary);
+                for (Map<String, Integer> dDay : allDDays) {
+                    for (Map.Entry<String, Integer> entry : dDay.entrySet()) {
+                        if (entry.getValue() == 0) {
+                            try {
+                                mailService.sendAnniversaryEmail(anniversary.getMember(), anniversary);
+                            } catch (MessagingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
