4d08e007be81acc215b4b880aeff76b42215cbe7
Lee junbeom <jach1206@naver.com>
2024-06-29 00:05:04 +0900

    비회원 전용 api들은 Spring security에 포함 되지 않도록 수정

--- src/main/java/com/example/fiurinee/domain/flower/repository/FlowerRepository.java
+++ src/main/java/com/example/fiurinee/domain/flower/repository/FlowerRepository.java
@@ -3,6 +3,7 @@ package com.example.fiurinee.domain.flower.repository;
 import com.example.fiurinee.domain.flower.entity.Flower;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.Optional;
 public interface FlowerRepository extends JpaRepository<Flower, Long> {
 
     @Query("SELECT f FROM Flower f WHERE f.period BETWEEN :startPeriod AND :endPeriod")
-    List<Flower> findByPeriodMonth(Long startPeriod, Long endPeriod);
+    List<Flower> findByPeriodMonth(@Param("startPeriod") Long startPeriod, @Param("endPeriod") Long endPeriod);
 
     Optional<Flower> findByNameAndFlowerLanguage(String name,String flowerLanguage);
 }
